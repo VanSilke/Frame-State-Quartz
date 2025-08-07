f875f52bd155c4a39f57f750ed1cbb792c06fec5
VanSilke <laoksiol@gmail.com>
2025-08-07 20:52:14 +0200

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "0. Landing",
+    pageTitle: "Tester",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
